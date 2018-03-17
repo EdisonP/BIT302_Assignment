32615be0d1cbf0f5ef771a5cca1aa526922e97bd
weishen1223 <31945615+weishen1223@users.noreply.github.com>
2018-03-18 06:58:57 +0800

    Update registerF.php

--- php/registerF.php
+++ php/registerF.php
@@ -1,33 +1,18 @@
 <?php
 include 'DBConfig.php';
 
-session_start();
-if (isset($_POST['register'])){
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "registernlogin";
-	$con = new mysqli($servername, $username, $password, $dbname);
-	
-	$name = $_POST['name'];
-	$pwd = $_POST['pwd'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password =  $_POST['password'];
+$cspassword = $_POST['cspassword'];
 
-	if($name != '' && $pwd != ''){
-		$select = "SELECT * FROM registernlogin WHERE username='$name'";
-		$selectR = mysqli_query($con,$select);
-		
-		if (mysqli_num_rows($selectR) == 0){
-			$_SESSION['name2'] = $name;
-			$sql = "INSERT INTO registernlogin(username,password) VALUES ('$name','$pwd')";
-			mysqli_query($con,$sql);
-			header('location:registerNlogin2.php');
-		}
-		else{
-			echo "You entered username '$name' is already existed.";			
-		}
-	}
-	else{
-		echo "Enter both username and password";
-	}
+if ($password == $cspassword){
+	$sql = "INSERT INTO `user` (`ID`, `username`, `email`, `password`)  VALUES (NULL, '$username', '$email', '$password')";
+	mysqli_query($conn, $sql);
+	header( "refresh:0.5;url=../index.html" );
+	echo ("<script>alert('Signed up successfully!')</script>");
+} else {
+	header( "refresh:0.5;url=../register.html" );
+	echo ("<script>alert('Password does not match with Confirm Password!')</script>");
 }
 ?>
