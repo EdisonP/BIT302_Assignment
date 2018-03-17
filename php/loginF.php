ae8cd59c64cb022e031fb15d3dda13f189a2632e
EdisonP <EdisonP@users.noreply.github.com>
2018-03-18 02:57:11 +0800

    Completed register and login function

---
+++ php/loginF.php
@@ -0,0 +1,19 @@
+<?php
+include 'DBConfig.php';
+
+$email = $_POST['email'];
+$password =  $_POST['password'];
+
+$sql = "SELECT * FROM `user` WHERE email='$email' and password ='$password'";
+$result = mysqli_query($conn, $sql);
+$numrows = mysqli_num_rows($result);
+
+if ($numrows == 1) {
+	header( "refresh:0.5;url=../index.html" );
+	echo ("<script>alert('Logged in successfully!')</script>");
+} else {
+	header( "refresh:0.5;url=../login.html" );
+	echo ("<script>alert('Invalid email or password!')</script>");
+}
+$conn->close();
+?>
--- php/registerF.php
+++ php/registerF.php
@@ -1,28 +1,18 @@
 <?php
-session_start();
 include 'DBConfig.php';
-
-// Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
 
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password =  $_POST['password'];
+$cspassword = $_POST['cspassword'];
 
-$sql = "INSERT INTO user (`ID`, `username`, `email`, `password`) VALUES (NULL, '$username', '$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-	echo "<script>
-	alert('Signed up successfully');
-	window.location.href='../index.html';
-	</script>";
+if ($password == $cspassword){
+	$sql = "INSERT INTO `user` (`ID`, `username`, `email`, `password`)  VALUES (NULL, '$username', '$email', '$password')";
+	mysqli_query($conn, $sql);
+	header( "refresh:0.5;url=../index.html" );
+	echo ("<script>alert('Signed up successfully!')</script>");
 } else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
+	header( "refresh:0.5;url=../register.html" );
+	echo ("<script>alert('Password does not match with Confirm Password!')</script>");
 }
-
-$conn->close();
 ?>
