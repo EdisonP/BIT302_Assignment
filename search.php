35f2fd3833cd2a59856979ae2120ae66e8fd4772
weishen1223 <31945615+weishen1223@users.noreply.github.com>
2018-03-28 11:50:10 +0800

    Add files via upload

---
+++ search.php
@@ -0,0 +1,90 @@
+<?php
+	session_start();
+
+	$search= $_GET['search']; 
+	
+	if($search == '') 
+		echo "you didn't submit a keyword"; 
+	else { 
+		if( strlen( $search )<= 1) 
+			echo "Search term too short 
+					</br>
+					<html>
+					<button onclick=\"goBack()\">Back</button>
+					<script>
+					
+					function goBack() {
+						window.history.back();
+					}
+					</script>
+					</body>
+					</html>
+					";
+		else { 
+			echo "You searched for <b> $search </b> <hr size='1'> </br>"; 	
+			$servername= "localhost";
+			$username = "root";		
+			$password= "";
+			$dbname= "user";
+			$con = new mysqli($servername,$username,$password,$dbname);
+				
+			$search_exploded = explode(" ", $search ); 
+			$x = 0; 
+			foreach($search_exploded as $search_each ) 
+			{
+				$x++; 
+				$construct = ""; 
+				if( $x == 1 ) 
+					$construct.="pagecontent LIKE '%$search_each%'"; 
+				else 
+					$construct.="AND pagecontent LIKE '%$search_each%'"; 
+			} 
+			
+			$construct= "SELECT * FROM searchEngine WHERE $construct"; 
+			$run=mysqli_query($con,$construct); 
+			
+			$foundnum=mysqli_num_rows($run); 
+			
+			if($foundnum == 0) 
+				echo "Sorry, there are no matching result for <b>$search</b>.</br> </br> 1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' </br> 2. Try different words with similar meaning </br> 3. Please check your spelling 
+					</br>
+					<html>
+					<button onclick=\"goBack()\">Back</button>
+					<script>
+					
+					function goBack() {
+						window.history.back();
+					}
+					</script>
+					</body>
+					</html>
+					";
+			else { 
+				echo "$foundnum results found !<p>";
+				while( $runrows = mysqli_fetch_assoc($run))
+					{
+						$title =$runrows['title']; 
+						$url =$runrows['pageurl'];
+						
+						echo "<a href='$url'> <b> $title
+						<br> <a href='$url'>$url </a> <p>"; 
+					}
+					echo "
+					<html>
+					<button onclick=\"goBack()\">Back</button>
+					<script>
+					function goBack() {
+						window.history.back();
+					}
+					</script>
+					</body>
+					</html>
+					";
+				 } 
+			} 
+		}
+
+
+	
+	
+?>
