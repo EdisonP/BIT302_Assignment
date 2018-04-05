347ba51f7b8bc20153a878d879b2def9da6b3f88
EdisonP <EdisonP@users.noreply.github.com>
2018-04-05 14:21:08 +0800

    Database 2.0

--- jucommunity.sql
+++ jucommunity.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2018 at 04:36 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 5.6.34
+-- Generation Time: Apr 05, 2018 at 08:20 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -32,20 +30,24 @@ CREATE TABLE `jobs` (
   `jobID` int(11) NOT NULL,
   `jobName` varchar(255) NOT NULL,
   `jobDetails` varchar(255) NOT NULL,
-  `jobClientName` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `salary` varchar(255) NOT NULL,
-  `jobStatus` tinyint(1) NOT NULL
+  `jobStatus` tinyint(1) NOT NULL,
+  `jobClientName` varchar(255) NOT NULL,
+  `jobClientID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jobs`
 --
 
-INSERT INTO `jobs` (`jobID`, `jobName`, `jobDetails`, `jobClientName`, `address`, `salary`, `jobStatus`) VALUES
-(1, 'test', 'test', 'test', 'test', 'RM100', 0),
-(3, 'asd', 'asd', 'asd', 'asd', 'RM200', 0),
-(4, 'test2', 'test2', 'test2', 'test2', 'RM422', 0);
+INSERT INTO `jobs` (`jobID`, `jobName`, `jobDetails`, `address`, `salary`, `jobStatus`, `jobClientName`, `jobClientID`) VALUES
+(1, ' tyu', 'tyu', 'tyu', 'tyu', 0, '', 1),
+(4, 'test2', 'test2', 'test2', 'RM422', 0, '', 1),
+(5, 'farmer', 'farmer', '444, aajalan 100', 'qqq', 0, '', 1),
+(6, 'asdf', 'asdf', 'asdf', 'asdf', 0, '', 1),
+(7, 'qwer', 'qwer', 'qwer', 'qwer', 0, '', 1),
+(8, 'zxc', 'zxc', 'zxc', 'zxc', 0, 'zxc', 1);
 
 -- --------------------------------------------------------
 
@@ -65,7 +67,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userID`, `username`, `email`, `password`) VALUES
-(1, 'asd', 'asd@asd.com', 'asd');
+(1, 'asd', 'asd@asd.com', 'asd'),
+(2, 'test', 'test@test.com', 'test'),
+(3, '123', '123@123.com', '123'),
+(4, 'edison', 'edison@gmail.com', '123');
 
 --
 -- Indexes for dumped tables
@@ -75,7 +80,8 @@ INSERT INTO `user` (`userID`, `username`, `email`, `password`) VALUES
 -- Indexes for table `jobs`
 --
 ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`jobID`);
+  ADD PRIMARY KEY (`jobID`),
+  ADD KEY `jobClientID` (`jobClientID`);
 
 --
 -- Indexes for table `user`
@@ -91,14 +97,21 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `jobID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `jobID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`jobClientID`) REFERENCES `user` (`userID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
