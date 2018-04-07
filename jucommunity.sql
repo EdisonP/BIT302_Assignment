c5f22aeada760d134a89bf0b38c01e76b72725bb
EdisonP <EdisonP@users.noreply.github.com>
2018-04-07 16:56:11 +0800

    new database

--- jucommunity.sql
+++ jucommunity.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2018 at 08:20 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Apr 07, 2018 at 10:55 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,97 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jobs`
+-- Table structure for table `jobapplication`
 --
 
-CREATE TABLE `jobs` (
-  `jobID` int(11) NOT NULL,
-  `jobName` varchar(255) NOT NULL,
-  `jobDetails` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `salary` varchar(255) NOT NULL,
-  `jobStatus` tinyint(1) NOT NULL,
-  `jobClientName` varchar(255) NOT NULL,
-  `jobClientID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jobs`
---
-
-INSERT INTO `jobs` (`jobID`, `jobName`, `jobDetails`, `address`, `salary`, `jobStatus`, `jobClientName`, `jobClientID`) VALUES
-(1, ' tyu', 'tyu', 'tyu', 'tyu', 0, '', 1),
-(4, 'test2', 'test2', 'test2', 'RM422', 0, '', 1),
-(5, 'farmer', 'farmer', '444, aajalan 100', 'qqq', 0, '', 1),
-(6, 'asdf', 'asdf', 'asdf', 'asdf', 0, '', 1),
-(7, 'qwer', 'qwer', 'qwer', 'qwer', 0, '', 1),
-(8, 'zxc', 'zxc', 'zxc', 'zxc', 0, 'zxc', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
+CREATE TABLE `jobapplication` (
+  `jobAppID` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `jobID` int(11) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `jobapplication`
 --
 
-INSERT INTO `user` (`userID`, `username`, `email`, `password`) VALUES
-(1, 'asd', 'asd@asd.com', 'asd'),
-(2, 'test', 'test@test.com', 'test'),
-(3, '123', '123@123.com', '123'),
-(4, 'edison', 'edison@gmail.com', '123');
+INSERT INTO `jobapplication` (`jobAppID`, `userID`, `jobID`, `date`) VALUES
+(1, 1, 1, '0000-00-00'),
+(2, 1, 4, '2018-04-07');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `jobs`
+-- Indexes for table `jobapplication`
 --
-ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`jobID`),
-  ADD KEY `jobClientID` (`jobClientID`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
+ALTER TABLE `jobapplication`
+  ADD PRIMARY KEY (`jobAppID`),
+  ADD KEY `jobID` (`jobID`),
+  ADD KEY `userID` (`userID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `jobs`
+-- AUTO_INCREMENT for table `jobapplication`
 --
-ALTER TABLE `jobs`
-  MODIFY `jobID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `jobapplication`
+  MODIFY `jobAppID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `jobs`
+-- Constraints for table `jobapplication`
 --
-ALTER TABLE `jobs`
-  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`jobClientID`) REFERENCES `user` (`userID`);
+ALTER TABLE `jobapplication`
+  ADD CONSTRAINT `jobapplication_ibfk_1` FOREIGN KEY (`jobID`) REFERENCES `jobs` (`jobID`),
+  ADD CONSTRAINT `jobapplication_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
